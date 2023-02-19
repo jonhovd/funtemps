04c0099775a92a07421633f1957d0e5af4083186
jonhovd <jonhovd@outlook.com>
2023-02-19 13:11:54 +0100

    main go ferdig, men fikk ikke til å fjerne ekstra desimal tall fra input til output.

--- main.go
+++ main.go
@@ -16,8 +16,6 @@ var out string
 
 //var funfacts string
 
-// Bruker init (som anbefalt i dokumentasjonen) for å sikre at flagvariablene
-// er initialisert.
 func init() {
 
 	//flag-variablen for F
@@ -31,9 +29,6 @@ func init() {
 
 	//flag.StringVar(&funfacts, "funfacts", "sun", "\"fun-facts\" om sun - Solen, luna - Månen og terra - Jorden")
 
-	// Du må selv definere flag-variabelen for -t flagget, som bestemmer
-	// hvilken temperaturskala skal brukes når funfacts skal vises
-
 }
 
 func main() {
@@ -43,42 +38,37 @@ func main() {
 	// Logikken for flaggene og kall til funksjonene fra conv:
 	if out == "C" && isFlagPassed("F") {
 		var celsius = conv.FarhenheitToCelsius(fahr)
-		//fmt.Printf("%#v°F is %.2f°C\n", fahr, celsius)
 		fmt.Printf("%.2f°F is %s°C\n", fahr, formatNumber(celsius))
 	}
 
 	if out == "F" && isFlagPassed("C") {
 		var fahrenheit = conv.CelsiusToFahrenheit(cel)
-		//fmt.Printf("%#v°C is %.2f°F\n", cel, fahrenheit)
 		fmt.Printf("%.2f°C is %s°F\n", cel, formatNumber(fahrenheit))
 	}
 
 	if out == "K" && isFlagPassed("C") {
 		var kelvin = conv.CelsiusToKelvin(cel)
-		//fmt.Printf("%#v°C is %.2f°K\n", cel, kelvin)
 		fmt.Printf("%.2f°C is %s°K\n", cel, formatNumber(kelvin))
 	}
 
 	if out == "C" && isFlagPassed("K") {
 		var celsius = conv.KelvinToCelsius(kel)
-		//fmt.Printf("%#v°K is %.2f°C\n", kel, celsius)
 		fmt.Printf("%.2f°K is %s°C\n", kel, formatNumber(celsius))
 	}
 
 	if out == "F" && isFlagPassed("K") {
 		var fahrenheit = conv.KelvinToFarhenheit(kel)
-		//fmt.Printf("%#v°K is %.2f°F\n", kel, fahrenheit)
 		fmt.Printf("%.2f°K is %s°F\n", kel, formatNumber(fahrenheit))
 	}
 
 	if out == "K" && isFlagPassed("F") {
 		var kelvin = conv.FahrenheitToKelvin(fahr)
-		//fmt.Printf("%#v°F is %.2f°K\n", fahr, kelvin)
 		fmt.Printf("%.2f°F is %s°K\n", fahr, formatNumber(kelvin))
 	}
 
 }
 
+// Funksjon for å ha mellomrom hver tredje siffer og fjerne desiamal tall etter konvertering
 func formatNumber(number float64) string {
 	numStr := fmt.Sprintf("%.2f", number)
 	numParts := strings.Split(numStr, ".")
